3f4af6e24e39d10db4d99918eee310c05e1d11f9
Harpo Roeder <roedeharpo@protonmail.ch>
2020-07-27 12:33:40 +0000

    doc strings for shared connection config

--- src/connection_config.rs
+++ src/connection_config.rs
@@ -36,6 +36,7 @@ pub struct NSQConfigShared {
 }
 
 impl NSQConfigShared {
+    /// Construct a configuration with sane defaults.
     pub fn new() -> Self {
         return NSQConfigShared {
             backoff_max_wait:      std::time::Duration::new(60, 0),
@@ -47,36 +48,37 @@ impl NSQConfigShared {
         }
     }
 
+    /// The maximum reconnect backoff wait. Defaults to 60 seconds.
     pub fn set_backoff_max_wait(mut self, duration: std::time::Duration) -> Self {
         self.backoff_max_wait = duration;
 
         return self;
     }
-
+    /// How long a connection should be healthy before backoff is reset. Defaults to 45 seconds.
     pub fn set_backoff_healthy_after(mut self, duration: std::time::Duration) -> Self {
         self.backoff_healthy_after = duration;
 
         return self;
     }
-
+    /// Connection compression options. Defaults to no compression.
     pub fn set_compression(mut self, compression: NSQConfigSharedCompression) -> Self {
         self.compression = Some(compression);
 
         return self;
     }
-
+    /// Credentials to send NSQD if authentication is requried. Defaults to no credentials.
     pub fn set_credentials(mut self, credentials: Vec<u8>) -> Self {
         self.credentials = Some(credentials);
 
         return self;
     }
-
+    /// Connection encryption options. Defaults to no encryption
     pub fn set_tls(mut self, tls: NSQConfigSharedTLS) -> Self {
         self.tls = Some(tls);
 
         return self;
     }
-
+    /// A string used to identify an NSQ client. Defaults to anonymous identity.
     pub fn set_client_id<S: Into<String>>(mut self, client_id: S) -> Self {
         self.client_id = Some(client_id.into());
 
