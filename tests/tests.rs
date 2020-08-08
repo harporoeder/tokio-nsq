955da1d2bbd600f095b234d7969fef219083342d
Harpo Roeder <roedeharpo@protonmail.ch>
2020-08-08 01:32:03 +0000

    add integration tests take 2

---
+++ tests/tests.rs
@@ -0,0 +1,77 @@
+extern crate rand;
+
+use tokio_nsq::*;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+use std::collections::HashSet;
+
+#[tokio::test]
+async fn basic_consume_direct() {
+    let rand_topic: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+
+    let topic   = NSQTopic::new(rand_topic).unwrap();
+    let channel = NSQChannel::new("first").unwrap();
+
+    let mut producer = NSQProducerConfig::new("127.0.0.1:4150").build();
+
+    let mut addresses = std::vec::Vec::new();
+    addresses.push("127.0.0.1:4150".to_string());
+
+    let _ = producer.consume().await;
+    producer.publish(&topic, "alice1".to_string().as_bytes().to_vec()).unwrap();
+
+    let mut consumer = NSQConsumerConfig::new(topic, channel)
+        .set_max_in_flight(1)
+        .set_sources(NSQConsumerConfigSources::Daemons(addresses))
+        .build();
+
+    let message = consumer.consume_filtered().await.unwrap();
+
+    assert_eq!(message.attempt, 1);
+    assert_eq!(message.body, b"alice1");
+
+    message.finish();
+}
+
+#[tokio::test]
+async fn basic_consume_lookup() {
+    let rand_topic: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+
+    let topic   = NSQTopic::new(rand_topic).unwrap();
+    let channel = NSQChannel::new("first").unwrap();
+
+    let mut producer = NSQProducerConfig::new("127.0.0.1:4150").build();
+
+    let mut addresses = std::vec::Vec::new();
+    addresses.push("127.0.0.1:4150".to_string());
+
+    let _ = producer.consume().await;
+    producer.publish(&topic, "alice1".to_string().as_bytes().to_vec()).unwrap();
+
+    let mut addresses = HashSet::new();
+    addresses.insert("http://127.0.0.1:4161".to_string());
+
+    let mut consumer = NSQConsumerConfig::new(topic, channel)
+        .set_max_in_flight(1)
+        .set_sources(
+            NSQConsumerConfigSources::Lookup(
+                NSQConsumerLookupConfig::new()
+                    .set_addresses(addresses)
+                    .set_poll_interval(std::time::Duration::from_millis(10))
+            )
+        )
+        .build();
+
+    let message = consumer.consume_filtered().await.unwrap();
+
+    assert_eq!(message.attempt, 1);
+    assert_eq!(message.body, b"alice1");
+
+    message.finish();
+}
